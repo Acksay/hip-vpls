05279a40816d9e9e7db4b033b6c795402c135ed1
faronsson <fabian.aronsson@gmail.com>
2024-12-09 12:06:28 +0100

    error

--- hipls-mn.py
+++ hipls-mn.py
@@ -26,19 +26,29 @@ class NetworkTopo( Topo ):
         router2 = self.addNode( 'r2', cls=LinuxRouter )
         router3 = self.addNode( 'r3', cls=LinuxRouter )
         router4 = self.addNode( 'r4', cls=LinuxRouter )
-        s1, s2, s3, s4, s5 = [ self.addSwitch( s, cls=OVSKernelSwitch ) for s in ( 's1', 's2', 's3', 's4', 's5' ) ]
-        self.addLink( s5, router1,
+        router5 = self.addNode( 'r4', cls=LinuxRouter )
+        router6 = self.addNode( 'r6', cls=LinuxRouter )
+
+        s1, s2, s3, s4, s5, s6, s7 = [ self.addSwitch( s, cls=OVSKernelSwitch ) for s in ( 's1', 's2', 's3', 's4', 's5','s6','s7' ) ]
+        self.addLink( s7, router1,
                 intfName2='r1-eth1',
                 params2={ 'ip' : '192.168.3.1/29' } )
-        self.addLink( s5, router2,
+        self.addLink( s7, router2,
                 intfName2='r2-eth1',
                 params2={ 'ip' : '192.168.3.2/29' } )
-        self.addLink( s5, router3,
+        self.addLink( s7, router3,
                 intfName2='r3-eth1',
                 params2={ 'ip' : '192.168.3.3/29' } )
-        self.addLink( s5, router4,
+        self.addLink( s7, router4,
                 intfName2='r4-eth1',
-                params2={ 'ip' : '192.168.3.3/29' } )
+                params2={ 'ip' : '192.168.3.4/29' } )
+        self.addLink( s7, router5,
+                intfName2='r5-eth1',
+                params2={ 'ip' : '192.168.3.5/29' } )
+        self.addLink( s7, router6,
+                intfName2='r6-eth1',
+                params2={ 'ip' : '192.168.3.6/29' } )
+        
         self.addLink( s1, router1, intfName2='r1-eth0',
                       params2={ 'ip' : '192.168.1.1/24' } )
         self.addLink( s2, router2, intfName2='r2-eth0',
@@ -47,6 +57,12 @@ class NetworkTopo( Topo ):
                 params2={ 'ip' : '192.168.1.3/24' } )
         self.addLink( s4, router4, intfName2='r4-eth0',
                 params2={ 'ip' : '192.168.1.4/24' } )
+        self.addLink( s5, router5, intfName2='r5-eth0',
+                params2={ 'ip' : '192.168.1.5/24' } )
+        self.addLink( s6, router6, intfName2='r6-eth0',
+                params2={ 'ip' : '192.168.1.6/24' } )
+        
+        
         h1 = self.addHost( 'h1', ip='192.168.1.100/24',
                            defaultRoute='via 192.168.1.1' )
         h2 = self.addHost( 'h2', ip='192.168.1.101/24',
@@ -55,7 +71,13 @@ class NetworkTopo( Topo ):
                            defaultRoute='via 192.168.1.1' )
         h4 = self.addHost( 'h4', ip='192.168.1.103/24',
                            defaultRoute='via 192.168.1.1' )
-        for h, s in [ (h1, s1), (h2, s2), (h3, s3), (h4, s4) ]:
+        h5 = self.addHost( 'h5', ip='192.168.1.104/24',
+                           defaultRoute='via 192.168.1.1' )
+        h6 = self.addHost( 'h6', ip='192.168.1.105/24',
+                           defaultRoute='via 192.168.1.1' )
+
+
+        for h, s in [ (h1, s1), (h2, s2), (h3, s3), (h4, s4), (h5,s5), (h6,s6) ]:
             self.addLink( h, s )
 from time import sleep
 def run():
@@ -66,31 +88,47 @@ def run():
     info( net[ 'r2' ].cmd( 'ifconfig r2-eth1 192.168.3.2 netmask 255.255.255.248' ) )
     info( net[ 'r3' ].cmd( 'ifconfig r3-eth1 192.168.3.3 netmask 255.255.255.248' ) )
     info( net[ 'r4' ].cmd( 'ifconfig r4-eth1 192.168.3.4 netmask 255.255.255.248' ) )
+    info( net[ 'r5' ].cmd( 'ifconfig r5-eth1 192.168.3.5 netmask 255.255.255.248' ) )
+    info( net[ 'r6' ].cmd( 'ifconfig r6-eth1 192.168.3.6 netmask 255.255.255.248' ) )
+
 
     info( net[ 'r1' ].cmd( '/sbin/ethtool -K r1-eth0 rx off tx off sg off' ) )
     info( net[ 'r1' ].cmd( '/sbin/ethtool -K r1-eth1 rx off tx off sg off' ) )
     info( net[ 'r2' ].cmd( '/sbin/ethtool -K r2-eth0 rx off tx off sg off' ) )
     info( net[ 'r2' ].cmd( '/sbin/ethtool -K r2-eth1 rx off tx off sg off' ) )
     info( net[ 'r3' ].cmd( '/sbin/ethtool -K r3-eth0 rx off tx off sg off' ) )
-    info( net[ 'r4' ].cmd( '/sbin/ethtool -K r3-eth1 rx off tx off sg off' ) )
+    info( net[ 'r3' ].cmd( '/sbin/ethtool -K r3-eth1 rx off tx off sg off' ) )
     info( net[ 'r4' ].cmd( '/sbin/ethtool -K r4-eth0 rx off tx off sg off' ) )
     info( net[ 'r4' ].cmd( '/sbin/ethtool -K r4-eth1 rx off tx off sg off' ) )
+    
+    info( net[ 'r5' ].cmd( '/sbin/ethtool -K r5-eth0 rx off tx off sg off' ) )
+    info( net[ 'r5' ].cmd( '/sbin/ethtool -K r5-eth1 rx off tx off sg off' ) )
+    info( net[ 'r6' ].cmd( '/sbin/ethtool -K r6-eth0 rx off tx off sg off' ) )
+    info( net[ 'r6' ].cmd( '/sbin/ethtool -K r6-eth1 rx off tx off sg off' ) )
+
 
     info( net[ 'h1' ].cmd( '/sbin/ethtool -K h1-eth0 rx off tx off sg off' ) )
     info( net[ 'h2' ].cmd( '/sbin/ethtool -K h2-eth0 rx off tx off sg off' ) )
     info( net[ 'h3' ].cmd( '/sbin/ethtool -K h3-eth0 rx off tx off sg off' ) )
     info( net[ 'h4' ].cmd( '/sbin/ethtool -K h4-eth0 rx off tx off sg off' ) )
+    info( net[ 'h5' ].cmd( '/sbin/ethtool -K h5-eth0 rx off tx off sg off' ) )
+    info( net[ 'h6' ].cmd( '/sbin/ethtool -K h6-eth0 rx off tx off sg off' ) )
 
 
     info( net[ 'h1' ].cmd( 'ifconfig h1-eth0 mtu 1400' ) )
     info( net[ 'h2' ].cmd( 'ifconfig h2-eth0 mtu 1400' ) )
     info( net[ 'h3' ].cmd( 'ifconfig h3-eth0 mtu 1400' ) )
     info( net[ 'h4' ].cmd( 'ifconfig h4-eth0 mtu 1400' ) )
+    info( net[ 'h5' ].cmd( 'ifconfig h4-eth0 mtu 1400' ) )
+    info( net[ 'h6' ].cmd( 'ifconfig h4-eth0 mtu 1400' ) )
+
 
     info( net[ 's1' ].cmd( 'ovs-vsctl set bridge s1 stp_enable=true' ) )
     info( net[ 's2' ].cmd( 'ovs-vsctl set bridge s2 stp_enable=true' ) )
     info( net[ 's3' ].cmd( 'ovs-vsctl set bridge s3 stp_enable=true' ) )
     info( net[ 's4' ].cmd( 'ovs-vsctl set bridge s4 stp_enable=true' ) )
+    info( net[ 's5' ].cmd( 'ovs-vsctl set bridge s5 stp_enable=true' ) )
+    info( net[ 's6' ].cmd( 'ovs-vsctl set bridge s6 stp_enable=true' ) )
 
 
     info( '*** Routing Table on Router:\n' )
@@ -105,6 +143,12 @@ def run():
     info( net[ 'r3' ].cmd( 'cd router3 && python3 switchd.py &' ) )
     info( '*** Running HIPLS on router 4 *** \n')
     info( net[ 'r4' ].cmd( 'cd router4 && python3 switchd.py &' ) )
+    
+    info( '*** Running HIPLS on router 5 *** \n')
+    info( net[ 'r5' ].cmd( 'cd router5 && python3 switchd.py &' ) )
+    info( '*** Running HIPLS on router 6 *** \n')
+    info( net[ 'r6' ].cmd( 'cd router6 && python3 switchd.py &' ) )
+
     CLI( net )
     net.stop()
 
